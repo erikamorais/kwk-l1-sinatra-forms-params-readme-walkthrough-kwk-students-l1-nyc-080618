aa9a996f74eef7682e33a56ba63113fab9a95f1b
erikamorais <erikamorais2001@gmail.com>
2018-08-15 13:47:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/results' do
-    erb :results
+  get '/' do
+    erb :fortune
   end
 
   # Add your post route and action below
